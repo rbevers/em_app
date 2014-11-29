a957c91f070510f78528cbc8ac54ee81512eb757
Russell Bevers <rbevers@acm.org>
2014-11-29 16:35:17 -0700

    Learned my lesson about returns in Procs.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,14 +7,16 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile << Proc.new do |path|
-  return false if path !~ /\.(css|js)\z/
-  full_path = Rails.application.assets.resolve(path).to_path
-  app_assets_path = Rails.root.join('app', 'assets').to_path
-  if full_path.starts_with? app_assets_path
-    puts "including asset: " + full_path
-    true
-  else
-    puts "excluding asset: " + full_path
-    false
+  result = false
+  if path =~ /\.(css|js)\z/
+    full_path = Rails.application.assets.resolve(path).to_path
+    app_assets_path = Rails.root.join('app', 'assets').to_path
+    if full_path.starts_with? app_assets_path
+      puts "including asset: " + full_path
+      result = true
+    else
+      puts "excluding asset: " + full_path
+    end
   end
+  result
 end
