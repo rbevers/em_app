8cd2ffc83b366f816c179e1dd3f3bf526e1356c7
Russell Bevers <rbevers@acm.org>
2014-11-30 14:02:51 -0700

    Added a fuller set of log in specs.

--- spec/features/as_as_user_i_want_to_log_in_spec.rb
+++ spec/features/as_as_user_i_want_to_log_in_spec.rb
@@ -1,18 +1,43 @@
 require 'rails_helper'
 
 feature 'As a User I want to log in so that I can begin utilizing the additional features of the application' do
+  EXPECTED_ADDL_FEATURES_CONTENT = 'additional features of the application'
+  EXPECTED_SUCCESS_MESSAGE = 'Signed in successfully.'
+
   background do
-    create(:user, email: 'ilike@homepage.com', password: 'abcDEF123ivvvi')
+    create(:user, email: 'iwanna@login.com', password: 'abcDEF123ivvvi')
   end
 
-  scenario 'navigates first to the home page' do
+  scenario 'initial hit to home page goes to log in page' do
     visit '/'
-    within('#new_user') do
-      fill_in 'Email', with: 'ilike@homepage.com'
-      fill_in 'Password', with: 'abcDEF123ivvvi'
-    end
+    expect(current_path).to eq('/users/sign_in')
+  end
+
+  scenario 'successful log in' do
+    visit '/users/sign_in'
+    fill_in 'Email', with: 'iwanna@login.com'
+    fill_in 'Password', with: 'abcDEF123ivvvi'
     click_button 'Log in'
     expect(page).to have_content 'Signed in successfully.'
     expect(page).to have_content 'additional features of the application'
   end
+
+  scenario 'failure due to bad password' do
+    visit '/users/sign_in'
+    fill_in 'Email', with: 'iwanna@login.com'
+    fill_in 'Password', with: 'abcDEF123456'
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid email or password.'
+    expect(page).to have_button 'Log in'
+  end
+
+  scenario 'failure due to unrecognized email' do
+    visit '/users/sign_in'
+    fill_in 'Email', with: 'whoami@login.com'
+    fill_in 'Password', with: 'abcDEF123ivvvi'
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid email or password.'
+    expect(page).to have_button 'Log in'
+  end
+
 end
