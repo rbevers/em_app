23c9d7d0b24759ed30c6f63f67f77a60699f230c
Russell Bevers <rbevers@acm.org>
2014-11-30 13:00:28 -0700

    Added Lenny to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(email: 'rbevers@acm.org', password: 'megaSh4rpC0d3r!')
+User.create(email: 'lmiller@emyth.com', password: 'aMi6htYfineJud63Ochar$')
